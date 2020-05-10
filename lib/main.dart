8254613b1a6da24eadce0b8d339ce05e3d31517f
ujjwal <ujjwalmaity98@gmail.com>
2020-05-10 13:41:28 +0530

    Navigate with named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'screen0.dart';
 import 'screen1.dart';
+import 'screen2.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Screen1(),
+      //home: Screen0(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Screen0(),
+        '/first': (context) => Screen1(),
+        '/second': (context) => Screen2(),
+      },
     );
   }
 }
